8826c218683309d452803be909d6806fbe890f79
Rishav Nandi <rishavnandi06@gmail.com>
2024-11-21 19:47:19 +0530

    Time: 1 ms (83.28%), Space: 51 MB (24.09%) - LeetHub

---
+++ 1528-shuffle-string/1528-shuffle-string.js
@@ -0,0 +1,12 @@
+/**
+ * @param {string} s
+ * @param {number[]} indices
+ * @return {string}
+ */
+var restoreString = function(s, indices) {
+    let res = Array(indices.length);
+    for(let i=0;i<indices.length;i++){
+        res[indices[i]] = s[i];
+    }
+    return res.join("");
+};
