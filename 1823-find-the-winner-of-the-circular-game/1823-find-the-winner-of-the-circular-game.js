608b420d01c65fda68513aa4d528307417281183
Rishav Nandi <rishavnandi06@gmail.com>
2025-01-07 20:54:45 +0530

    Time: 0 ms (100%), Space: 48.7 MB (86.77%) - LeetHub

--- 1823-find-the-winner-of-the-circular-game/1823-find-the-winner-of-the-circular-game.js
+++ 1823-find-the-winner-of-the-circular-game/1823-find-the-winner-of-the-circular-game.js
@@ -4,15 +4,12 @@
  * @return {number}
  */
 var findTheWinner = function(n, k) {
-    const arr = Array.from({length: n}, (_,i) => i + 1);
-    
-    function helper(arr,startIdx) {
-        // base case
-        if(arr.length === 1) return arr[0];
-        // rec case
-        let indexToDelete = (startIdx + k - 1) % arr.length;
-        arr.splice(indexToDelete,1);
-        return helper(arr,indexToDelete);
+    function helper(n){
+        if(n === 1){
+            return 0;
+        }else {
+            return (helper(n-1) + k) % n;
+        }
     }
-    return helper(arr,0);
+    return helper(n) + 1;
 };
