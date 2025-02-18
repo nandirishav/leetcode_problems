f603c7698eaba94695329368e9239a816e074e2d
Rishav Nandi <rishavnandi06@gmail.com>
2025-02-18 19:03:43 +0530

    Time: 4 ms (41.75%), Space: 59.6 MB (5.3%) - LeetHub

---
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.js
@@ -0,0 +1,27 @@
+/**
+ * @param {number[][]} matrix
+ * @return {void} Do not return anything, modify matrix in-place instead.
+ */
+var setZeroes = function(matrix) {
+    const m = matrix.length;
+    const n = matrix[0].length;
+    const row = Array(m).fill(0);
+    const col = Array(n).fill(0);
+
+    for(let i=0;i<m;i++) {
+        for(let j=0;j<n;j++){
+            if(matrix[i][j] === 0) {
+                row[i] = 1;
+                col[j] = 1;
+            }
+        }
+    }
+
+    for(let i=0;i<m;i++) {
+        for(let j=0;j<n;j++){
+            if(row[i] || col[j]) {
+                matrix[i][j] = 0;
+            }
+        }
+    }
+};
