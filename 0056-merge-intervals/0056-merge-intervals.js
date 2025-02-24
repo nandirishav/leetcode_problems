75246429d31fc37d59997e9b935238ea4c708829
Rishav Nandi <rishavnandi06@gmail.com>
2025-02-24 23:13:55 +0530

    Time: 6 ms (90.64%), Space: 63.6 MB (7.68%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.js
+++ 0056-merge-intervals/0056-merge-intervals.js
@@ -3,20 +3,23 @@
  * @return {number[][]}
  */
 var merge = function(intervals) {
+    const n = intervals.length;
+    // sort the given intervals
     intervals.sort((a, b) => a[0] - b[0]);
-    const merged = [];
-    let prev = intervals[0];
-
-    for (let i = 1; i < intervals.length; i++) {
-        let interval = intervals[i];
-        if (interval[0] <= prev[1]) {
-            prev[1] = Math.max(prev[1], interval[1]);
-        } else {
-            merged.push(prev);
-            prev = interval;
+    const ans = [intervals[0]];
+    
+    for (let i = 1; i < n; i++) {
+        const last = ans[ans.length - 1];
+        const curr = intervals[i];        
+        // if the current interval overlaps with the last interval
+        if (curr[0] <= last[1]) {
+            last[1] = Math.max(last[1], curr[1]);
+        }
+        // if the current interval does not overlap with the last interval
+        else {
+            ans.push(curr);
         }
     }
-
-    merged.push(prev);
-    return merged;    
+  
+  return ans;
 };
