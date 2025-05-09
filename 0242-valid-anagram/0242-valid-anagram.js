e08a002889ba06381352288250f076a53e1458e3
Rishav Nandi <rishavnandi06@gmail.com>
2025-05-09 20:22:20 +0530

    Time: 19 ms (41.26%), Space: 51.9 MB (100%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.js
+++ 0242-valid-anagram/0242-valid-anagram.js
@@ -7,5 +7,17 @@ var isAnagram = function(s, t) {
     if(s.length !== t.length){
         return false;
     }
-    return s.split('').sort().join('') === t.split('').sort().join('');
+    const map = new Map();
+    for(let i=0;i<s.length;i++){
+        map.set(s[i], (map.get(s[i]) || 0) + 1);
+    }
+    let res = 0;
+    for(let i=0;i<t.length;i++){
+        if(map.get(t[i])){
+            map.set(t[i], map.get(t[i]) - 1);
+        } else{
+            return false;
+        }
+    }
+    return true;
 };
