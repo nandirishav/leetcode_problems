4f7cb4fe0e69d25fbcc56590e71cd5436746cf0b
Rishav Nandi <rishavnandi06@gmail.com>
2025-05-09 19:48:58 +0530

    Time: 2 ms (11.27%), Space: 53.8 MB (54.44%) - LeetHub

--- 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.js
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.js
@@ -4,5 +4,20 @@
  * @return {number}
  */
 var strStr = function(haystack, needle) {
-    return haystack.indexOf(needle);
+    if(needle == ""){
+        return 0;
+    }
+    const n = haystack.length;
+    const m = needle.length;
+    for(let i=0;i < n - m + 1; i++) {
+        for(let j=0;j < m;j++){
+            if(haystack[i+j] !== needle[j]){
+                break;
+            }
+            if(j === needle.length -1){
+                return i;
+            }
+        }
+    }
+    return -1;
 };
