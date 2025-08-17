2a6bf1b96407f1673ba64ebaca5e81514c80095e
Rishav Nandi <rishavnandi06@gmail.com>
2025-08-17 18:38:49 +0530

    Time: 3 ms (53.18%), Space: 56.6 MB (71.52%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.js
+++ 0547-number-of-provinces/0547-number-of-provinces.js
@@ -2,35 +2,40 @@
  * @param {number[][]} isConnected
  * @return {number}
  */
-
-function dfs(vis, adj, node) {
-    vis[node] = 1;
-    
-    for(const item of adj[node]) {
-        if(!vis[item]) {
-            dfs(vis, adj, item);
-        }
-    }
-}
-
 var findCircleNum = function(isConnected) {
     const V = isConnected.length;
-    const adjList = Array.from({length: V + 1}, () => []);
+    const adjList = Array.from({length: V}, () => []);
     for(let i=0;i<V;i++) {
-        for(let j=0;j<V;j++){
+        for(let j=0;j<V;j++) {
             if(isConnected[i][j] == 1 && i !== j) {
                 adjList[i].push(j);
-                adjList[j].push(i);
             }
         }
     }
-    const vis = Array.from({length: V + 1}, () => 0);
+    const vis = Array(V).fill(0);
+    const queue = [];
     let provinces = 0;
-    for(let i=0;i<V;i++){
+
+    for(let i=0;i<V;i++) {
         if(!vis[i]) {
             provinces++;
-            dfs(vis, adjList, i);
+            bfs(vis, adjList, i);
         }
     }
     return provinces;
 };
+
+const bfs = function (vis, adj, node) {
+    vis[node] = 1;
+    const queue = [node];
+    
+    while(queue.length) {
+        const node = queue.shift();
+        for(const item of adj[node]) {
+            if(!vis[item]) {
+                queue.push(item);
+                vis[item] = 1;
+            }
+        }
+    }
+}
