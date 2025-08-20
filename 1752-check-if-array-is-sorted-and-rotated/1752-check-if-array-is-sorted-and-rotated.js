26a20fd11decddac8f7e0f421d62566cb50576e7
Rishav Nandi <rishavnandi06@gmail.com>
2025-08-21 01:46:54 +0530

    Time: 0 ms (100%), Space: 53.9 MB (35.04%) - LeetHub

---
+++ 1752-check-if-array-is-sorted-and-rotated/1752-check-if-array-is-sorted-and-rotated.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var check = function(nums) {
+    const n = nums.length;
+    let count = 0;
+    for(let i=0;i<n;i++) {
+        if(nums[i] > nums[(i+1) % n]) {
+            count++;
+        }
+    }
+    return count <= 1;
+};
