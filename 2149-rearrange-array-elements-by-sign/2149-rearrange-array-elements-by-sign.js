2035a828e058ddc6fba93309938a93a152554d73
Rishav Nandi <rishavnandi06@gmail.com>
2025-08-26 23:07:23 +0530

    Time: 27 ms (36.65%), Space: 96.1 MB (73.55%) - LeetHub

--- 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.js
+++ 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.js
@@ -3,21 +3,32 @@
  * @return {number[]}
  */
 var rearrangeArray = function(nums) {
-    const positives = [];
-    const negatives = [];
-    for(const num of nums) {
-        if(num < 0) {
-            negatives.push(num);
-        }else {
-            positives.push(num);
-        }
+   const res = [];
+   let positive = 0, negative = 1;
+   let i=0;
+   const n = nums.length;
+   while(i < n && positive < n && negative < n) {
+    const num = nums[i];
+    if(num > 0) {
+        res[positive] = num;
+        positive += 2;
+    }else {
+        res[negative] = num;
+        negative += 2;
     }
-    const res = [];
-    let i=0;
-    while(i < positives.length && i < negatives.length) {
-        res.push(positives[i]);
-        res.push(negatives[i]);
-        i++;
-    }
-    return res;
+    i++;
+   }
+   while(i < n && positive < n) {
+    const num = nums[i];
+    res[positive] = num;
+    positive += 2;
+    i++;
+   }
+   while(i < n && negative < n) {
+    const num = nums[i];
+    res[negative] = num;
+    negative += 2;
+    i++;
+   }
+   return res;
 };
